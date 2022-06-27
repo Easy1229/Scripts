03502253e20e7004fa31c107313a84bbcb0854d4
Easy1229 <101702525+Easy1229@users.noreply.github.com>
2022-06-27 17:42:13 +0800

    Add files via upload
    第一次上传

---
+++ C#进阶/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 继承
+{
+    class Student
+    {
+        public string name;
+        public int age;
+        public Gender gender;
+
+        public Student(string name, int age, Gender gender)
+        {
+            name = this.name;
+            age = this.age;
+            gender = this.gender;
+        }
+    }
+
+    enum Gender
+    {
+        Male,
+        Female,
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
