6562d593a6051e8f5f06b808d3ccb1fccf7828de
Easy1229 <101702525+Easy1229@users.noreply.github.com>
2022-06-27 23:36:33 +0800

    Add files via upload
    新写了订单系统

---
+++ Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace 继承
+{
+    enum Gender
+    {
+        Male,
+        Female,
+    }
+    class Student
+    {
+        public virtual void Say()
+        {
+            Console.WriteLine("123");
+        }
+       
+    }
+
+    class Test:Student
+    {
+        public override void Say()
+        {
+            Console.WriteLine("456");
+        }
+    }
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+            Test test = new Test();
+            student.Say();
+            test.Say();
+            
+        }
+    }
+}
